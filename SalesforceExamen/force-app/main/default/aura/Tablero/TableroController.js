7d02dad3a15d5d2b133692eb2771159eac960755
Samutitus <75837399+Samutitus@users.noreply.github.com>
2023-03-07 20:43:16 +0100

    Add files via upload

--- SalesforceExamen/force-app/main/default/aura/Tablero/TableroController.js
+++ SalesforceExamen/force-app/main/default/aura/Tablero/TableroController.js
@@ -1,8 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
-
+    init: function(component, event, helper) {
+        component.set('v.isClicked1', false);
+        component.set('v.isClicked2', false);
     },
     handleButton1Click : function(component, event, helper) {
         helper.toggleDiv(component);
-    },
+    }
 })
--- SalesforceExamen/force-app/main/default/aura/Tablero/TableroHelper.js
+++ SalesforceExamen/force-app/main/default/aura/Tablero/TableroHelper.js
@@ -1,6 +1,7 @@
 ({
     toggleDiv : function(component) {
-        var isClicked = component.get('v.isClicked');
+        var isClicked1 = component.get('v.isClicked1');
+        var isClicked2 = component.get('v.isClicked2');
         var divNum = Math.floor(Math.random() * 2) + 1;
         component.set('v.isClicked' + divNum, true);
         setTimeout($A.getCallback(function() {
